7c59271b990f762c66a5c2814c12e27965456329
Akeemolatunde <olatundea4@gmail.com>
2023-07-23 01:23:36 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,10 +16,10 @@ void times_table(void)
 		{
 			result = i * j;
 			if (j == 0)
-				printf("%d, ", result);
+				printf("%d", result);
 			else
 			{
-				printf("2%d, ", result);
+				printf(", %2d", result);
 				if (j != 9)
 					printf(", ");
 			}
