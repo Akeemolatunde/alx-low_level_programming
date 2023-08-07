cbe7307f43e68baacde537e9f947626affa64e1e
Akeemolatunde <olatundea4@gmail.com>
2023-08-07 10:41:33 -0700

    0-create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * craete_array - creates an array of chars
+ * @size: the size of the array
+ * @c: the char to be filled in the array
+ * Return: the array filled
+ */
+char *craete_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+
+	return (s);
+}
