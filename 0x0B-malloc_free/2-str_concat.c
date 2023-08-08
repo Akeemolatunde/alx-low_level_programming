5f18c9f0314829945bdb449592d97ca9dc0957a3
Akeemolatunde <olatundea4@gmail.com>
2023-08-08 05:58:58 -0700

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
-		ci++;
+		i++, ci++;
 	conct = malloc(sizeof(char) * (i = ci + 1));
 
 	if (conct == NULL)
@@ -36,7 +36,7 @@ char *str_concat(char *s1, char *s2)
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
-		ci++;
+		i++, ci++;
 	}
 	conct[i] = '\0';
 	return (conct);
