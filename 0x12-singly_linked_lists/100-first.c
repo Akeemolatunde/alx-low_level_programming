fd210c16e1aad465d646da7d06f29907a41cc41b
Akeemolatunde <olatundea4@gmail.com>
2023-08-26 08:34:15 -0700

    100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+ComeFirstbyCTC(void)__attribute__((constructor));
+
+/**
+ * ComeFirstbyCTC - Function that runs before the main function
+ */
+
+void ComeFirstbyCTC(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I boore my house upon my back!\n");
+}
