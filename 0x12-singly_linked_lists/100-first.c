8f8e179f4fba57db660254cfab61e1eff24a9d69
Akeemolatunde <olatundea4@gmail.com>
2023-08-27 07:22:41 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,17 @@
 #include "lists.h"
-ComeFirstbyCTC(void)__attribute__((constructor));
 
 /**
- * ComeFirstbyCTC - Function that runs before the main function
+ * myStartupFun - apply constructor attribute to mySarupFun()
+ * and execute before main()
+ */
+void myStartupFun(void)__attribute__((constructor));
+
+/**
+ * myStartupFun - implimentation of myStartupFun
  */
 
-void ComeFirstbyCTC(void)
+void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I boore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
